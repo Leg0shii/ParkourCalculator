b5368cea7358a72a7eea12e00903000ca845c1aa
0hnezahn <benjamin.mueller2@uni-rostock.de>
2023-04-25 12:56:23 +0200

    fix cocoa bean hit box by 0.005

--- src/main/java/de/legoshi/parkourcalculator/parkour/environment/blocks/CocoaBean.java
+++ src/main/java/de/legoshi/parkourcalculator/parkour/environment/blocks/CocoaBean.java
@@ -36,33 +36,33 @@ public class CocoaBean extends FacingBlock implements TierBlock {
 
     @Override
     protected void calcSouth() {
-        Vec3 lowerEdge = new Vec3(0.375 - addedSize, 0.4325 - addedSize * 2, 0.6875 - addedSize * 2);
+        Vec3 lowerEdge = new Vec3(0.375 - addedSize, 0.4375 - addedSize * 2, 0.6875 - addedSize * 2);
         Vec3 upperEdge = new Vec3(0.625 + addedSize, 0.75, 0.9375);
-        Vec3 shift = new Vec3(0.375 - addedSize, 0.3425 - addedSize, 0.375 - addedSize);
+        Vec3 shift = new Vec3(0.375 - addedSize, 0.3375 - addedSize, 0.375 - addedSize);
         this.axisVecTuples.add(constructBlock(lowerEdge, upperEdge, shift));
     }
 
     @Override
     protected void calcWest() {
-        Vec3 lowerEdge = new Vec3(0.6875 - addedSize * 2, 0.4325 - addedSize * 2, 0.375 - addedSize);
+        Vec3 lowerEdge = new Vec3(0.6875 - addedSize * 2, 0.4375 - addedSize * 2, 0.375 - addedSize);
         Vec3 upperEdge = new Vec3(0.9375, 0.75, 0.625 + addedSize);
-        Vec3 shift = new Vec3(0.375 - addedSize, 0.3425 - addedSize, 0.375 - addedSize);
+        Vec3 shift = new Vec3(0.375 - addedSize, 0.3375 - addedSize, 0.375 - addedSize);
         this.axisVecTuples.add(constructBlock(lowerEdge, upperEdge, shift));
     }
 
     @Override
     protected void calcNorth() {
-        Vec3 lowerEdge = new Vec3(0.375 - addedSize, 0.4325 - addedSize * 2, 0.0625);
+        Vec3 lowerEdge = new Vec3(0.375 - addedSize, 0.4375 - addedSize * 2, 0.0625);
         Vec3 upperEdge = new Vec3(0.625 + addedSize, 0.75, 0.3125 + addedSize * 2);
-        Vec3 shift = new Vec3(0.375 - addedSize, 0.3425 - addedSize, 0.375 - addedSize);
+        Vec3 shift = new Vec3(0.375 - addedSize, 0.3375 - addedSize, 0.375 - addedSize);
         this.axisVecTuples.add(constructBlock(lowerEdge, upperEdge, shift));
     }
 
     @Override
     protected void calcEast() {
-        Vec3 lowerEdge = new Vec3(0.0625, 0.4325 - addedSize * 2, 0.375 - addedSize);
+        Vec3 lowerEdge = new Vec3(0.0625, 0.4375 - addedSize * 2, 0.375 - addedSize);
         Vec3 upperEdge = new Vec3(0.3125 + addedSize * 2, 0.75, 0.625 + addedSize);
-        Vec3 shift = new Vec3(0.375 - addedSize, 0.3425 - addedSize, 0.375 - addedSize);
+        Vec3 shift = new Vec3(0.375 - addedSize, 0.3375 - addedSize, 0.375 - addedSize);
         this.axisVecTuples.add(constructBlock(lowerEdge, upperEdge, shift));
     }
 
